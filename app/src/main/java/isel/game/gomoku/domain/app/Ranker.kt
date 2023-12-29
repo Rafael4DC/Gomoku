9904191e0a603b8ac55febe30d5db175d756c340
48315 - Rafael Costa <a48315@alunos.isel.pt>
2023-12-29 18:41:37 +0000

    Project redone

--- app/src/main/java/isel/game/gomoku/domain/app/Ranker.kt
+++
@@ -1,7 +0,0 @@
-package isel.game.gomoku.domain.app
-
-data class Ranker (val username: String, val score: Int,val totalGames:Int){
-    init {
-        require(username.isNotBlank()) { "The username must not be blank" }
-    }
-}
